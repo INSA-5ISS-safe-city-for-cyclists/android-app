0ed21585e2f4cc1a4263eb0e70241690b2d1c0ca
Arnaud Vergnet <arnaud.vergnet@mailo.com>
2021-11-20 17:56:09 +0100

    feat: add maplibre using jawg server

--- app/src/main/java/com/insa/iss/safecityforcyclists/MainActivity.kt
+++ app/src/main/java/com/insa/iss/safecityforcyclists/MainActivity.kt
@@ -2,10 +2,73 @@ package com.insa.iss.safecityforcyclists
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.maps.MapView
 
 class MainActivity : AppCompatActivity() {
+
+    private var mapView: MapView? = null
+    private var customStyleId = "a66e810d-8e3a-4cb0-84b0-92ed63959ed4"
+
+    // Returns the Jawg url depending on the style given (jawg-streets by default)
+    // See /res/values/strings which contains the url, the list of styles and your access token.
+    private fun makeStyleUrl(style: String = "jawg-streets"): String {
+        return "${getString(R.string.jawg_styles_url) + style}.json?access-token=${getString(R.string.jawg_access_token)}";
+    }
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Get the MapBox context
+        Mapbox.getInstance(this)
+
         setContentView(R.layout.activity_main)
+
+        // Create map view
+        mapView = findViewById(R.id.mapView)
+        mapView?.onCreate(savedInstanceState)
+        mapView?.getMapAsync { map ->
+            map.setStyle(makeStyleUrl(customStyleId)) {
+                // Map fully loaded in this scope.
+                // Update attributions position
+                map.uiSettings.setAttributionMargins(15, 0, 0, 15)
+            }
+        }
     }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView?.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView?.onDestroy()
+    }
+
 }
