9e93dad2b88f7539153bed28d7e7a5110e060a82
Arnaud Vergnet <arnaud.vergnet@mailo.com>
2021-12-01 11:17:38 +0100

    feat: improve debug controls

--- app/src/main/java/com/insa/iss/safecityforcyclists/debug/fragments/DebugDatabaseFragment.kt
+++ app/src/main/java/com/insa/iss/safecityforcyclists/debug/fragments/DebugDatabaseFragment.kt
@@ -1,31 +1,83 @@
 package com.insa.iss.safecityforcyclists.debug.fragments
 
-import androidx.fragment.app.Fragment
-import com.insa.iss.safecityforcyclists.R
-
-import android.view.View
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.insa.iss.safecityforcyclists.R
+import com.insa.iss.safecityforcyclists.database.LocalReport
 import com.insa.iss.safecityforcyclists.reports.LocalDangerReportsViewModel
 
 class DebugDatabaseFragment : Fragment(R.layout.database_debug) {
 
     private val localDangerReportsViewModel: LocalDangerReportsViewModel by activityViewModels()
+    private var quickRemoveButton: FloatingActionButton? = null
+    private var deleteReportFAB: FloatingActionButton? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private fun setRemoveButtonState(enabled: Boolean) {
+        quickRemoveButton?.isEnabled = enabled
+        deleteReportFAB?.isEnabled = enabled
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val addReportFab: View = view.findViewById(R.id.addReportFAB)
-        addReportFab.setOnClickListener { _ ->
+        quickRemoveButton = view.findViewById(R.id.quickRemoveFAB)
+        deleteReportFAB = view.findViewById(R.id.deleteReportFAB)
+
+        localDangerReportsViewModel.getFeatures().value?.features()?.size?.let { size ->
+            setRemoveButtonState(size > 0)
+        }
+
+        localDangerReportsViewModel.getFeatures().observe(viewLifecycleOwner, { featureCollection ->
+            featureCollection?.features()?.size?.let { size ->
+                setRemoveButtonState(size > 0)
+            }
+        })
+
+        // Add dummy report
+        view.findViewById<FloatingActionButton>(R.id.quickAddFAB).setOnClickListener {
+            var size = localDangerReportsViewModel.getFeatures().value?.features()?.size
+            if (size == null) {
+                size = 0
+            }
+            localDangerReportsViewModel.addReports(
+                listOf(
+                    LocalReport(
+                        timestamp = size + 1,
+                        distance = (size + 2).toDouble(),
+                        objectSpeed = (size + 3).toDouble(),
+                        bicycleSpeed =(size + 4).toDouble(),
+                        latitude = (size + 5).toDouble(),
+                        longitude = (size + 6).toDouble(),
+                        sync = false
+                    )
+                )
+            )
+        }
+
+        // Remove last report
+        quickRemoveButton?.setOnClickListener {
+            localDangerReportsViewModel.getFeatures().value?.features()?.let { features ->
+                if (features.size > 0) {
+                    features.last().properties()?.get("id")?.asInt?.let { id ->
+                        localDangerReportsViewModel.deleteReportsById(listOf(id))
+                    }
+                }
+            }
+        }
+
+        view.findViewById<FloatingActionButton>(R.id.addReportFAB).setOnClickListener {
             val dialog = DeubgAddReportDialogFragment()
             dialog.show(parentFragmentManager, "Dialog")
         }
 
-        val viewReportFab: View = view.findViewById(R.id.viewReportFAB)
-        viewReportFab.setOnClickListener { _ ->
+        deleteReportFAB?.setOnClickListener {
+            val dialog = DebugDeleteReportDialogFragment()
+            dialog.show(parentFragmentManager, "Dialog")
+        }
+
+        view.findViewById<FloatingActionButton>(R.id.viewReportFAB).setOnClickListener {
             val result = viewRecord()
             val builder = AlertDialog.Builder(view.context)
             builder.setTitle("View reports")
@@ -33,19 +85,11 @@ class DebugDatabaseFragment : Fragment(R.layout.database_debug) {
             builder.show()
         }
 
-        val deleteReportFab: View = view.findViewById(R.id.deleteReportFAB)
-        deleteReportFab.setOnClickListener { _ ->
-            val dialog = DebugDeleteReportDialogFragment()
-            dialog.show(parentFragmentManager, "Dialog")
-        }
-
-        val syncAllReportsFab: View = view.findViewById(R.id.syncAllReportsFAB)
-        syncAllReportsFab.setOnClickListener { _ ->
+        view.findViewById<FloatingActionButton>(R.id.syncAllReportsFAB).setOnClickListener {
             localDangerReportsViewModel.syncReportsById((0..500).toList())
         }
 
-        val unsyncAllReportsFab: View = view.findViewById(R.id.unsyncAllReportsFAB)
-        unsyncAllReportsFab.setOnClickListener { _ ->
+        view.findViewById<FloatingActionButton>(R.id.unsyncAllReportsFAB).setOnClickListener {
             localDangerReportsViewModel.unsyncReportsById((0..500).toList())
         }
 
