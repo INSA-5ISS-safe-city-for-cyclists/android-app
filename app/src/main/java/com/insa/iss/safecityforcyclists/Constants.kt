9b4e892756f08337beb6fe52cfad088af5deb614
PiKouri <62264277+PiKouri@users.noreply.github.com>
2022-01-04 21:11:16 +0100

    fix: removed done TODO

--- app/src/main/java/com/insa/iss/safecityforcyclists/Constants.kt
+++ app/src/main/java/com/insa/iss/safecityforcyclists/Constants.kt
@@ -3,7 +3,6 @@ package com.insa.iss.safecityforcyclists
 class Constants {
     companion object {
         private const val API_URL = "http://0.0.0.0:3000/api/"
-        // TODO Only dangerous (?dangerous=1)
         const val API_ZONES_ENDPOINT = "${API_URL}zones"
         const val API_REPORTS_ENDPOINT = "${API_URL}reports"
         const val API_CRITERIA_ENDPOINT = "${API_URL}criteria"
