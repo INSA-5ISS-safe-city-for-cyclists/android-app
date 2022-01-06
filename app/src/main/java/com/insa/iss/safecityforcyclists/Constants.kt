c867c4d063efb5e4fe30e9e5a37e32f6526e8a07
Arnaud Vergnet <arnaud.vergnet@mailo.com>
2022-01-06 09:38:37 +0100

    feat: use https for the api

--- app/src/main/java/com/insa/iss/safecityforcyclists/Constants.kt
+++ app/src/main/java/com/insa/iss/safecityforcyclists/Constants.kt
@@ -2,7 +2,7 @@ package com.insa.iss.safecityforcyclists
 
 class Constants {
     companion object {
-        private const val API_URL = "http://0.0.0.0:3000/api/"
+        private const val API_URL = "https://rasp.pikouri.fr/api/"
         const val API_ZONES_ENDPOINT = "${API_URL}zones"
         const val API_REPORTS_ENDPOINT = "${API_URL}reports"
         const val API_CRITERIA_ENDPOINT = "${API_URL}criteria"
